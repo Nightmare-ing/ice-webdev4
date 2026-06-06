b87b6e658365c42b40b6d650e3f0c124be634cba
Qingsong Hu <kingsanhu@gmail.com>
2026-06-06 23:53:25 +0800

    Finish practice 1 of web dev4
    Send login username and password with `fetch`
    Also change URL of API to cs571.org, which is open to public.

--- starter/src/components/LoginOrCreatePost.jsx
+++ starter/src/components/LoginOrCreatePost.jsx
@@ -2,20 +2,39 @@ import { useRef, useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
 export default function LoginOrCreatePost(props) {
-    
     // Note! You should use this in combination with sessionStorage.
     // Otherwise, when the user refreshes the page, it will go away!
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
     function handleLoginSubmit(e) {
-        e?.preventDefault();  // prevents default form submit action
+        e?.preventDefault(); // prevents default form submit action
 
-        // TODO: POST to https://cs571api.cs.wisc.edu/rest/s25/ice/login
+        fetch("https://cs571.org/rest/s25/ice/login", {
+            method: "POST",
+            headers: {
+                "X-CS571-ID": CS571.getBadgerId(),
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            }),
+        }).then((res) => {
+            if (res.status === 401) {
+                alert("Invalid username or password!");
+            } else if (res.status === 200) {
+                alert("Successfully logged in!");
+            } else {
+                alert("Something happened!");
+            }
+        });
     }
 
     function handleCommentSubmit(e) {
         e?.preventDefault(); // prevents default form submit action
-        
+
         // TODO: POST to https://cs571api.cs.wisc.edu/rest/s25/ice/comments
     }
 
@@ -24,23 +43,46 @@ export default function LoginOrCreatePost(props) {
     }
 
     if (isLoggedIn) {
-        return <>
-            <Button variant="danger" onClick={handleLogout}>Logout</Button>
-            <Form onSubmit={handleCommentSubmit}>
-                <Form.Label htmlFor="commentInput">Your Comment</Form.Label>
-                <Form.Control id="commentInput"></Form.Control>
-                <br/>
-                <Button type="submit" onClick={handleCommentSubmit}>Post Comment</Button>
-            </Form>
-        </>
+        return (
+            <>
+                <Button variant="danger" onClick={handleLogout}>
+                    Logout
+                </Button>
+                <Form onSubmit={handleCommentSubmit}>
+                    <Form.Label htmlFor="commentInput">Your Comment</Form.Label>
+                    <Form.Control id="commentInput"></Form.Control>
+                    <br />
+                    <Button type="submit" onClick={handleCommentSubmit}>
+                        Post Comment
+                    </Button>
+                </Form>
+            </>
+        );
     } else {
-        return <Form onSubmit={handleLoginSubmit}>
-            <Form.Label htmlFor="usernameInput">Username</Form.Label>
-            <Form.Control id="usernameInput"></Form.Control>
-            <Form.Label htmlFor="passwordInput">Password</Form.Label>
-            <Form.Control id="passwordInput" type="password"></Form.Control>
-            <br/>
-            <Button type="submit" onClick={handleLoginSubmit}>Login</Button>
-        </Form>
+        return (
+            <Form onSubmit={handleLoginSubmit}>
+                <Form.Label htmlFor="usernameInput">Username</Form.Label>
+                <Form.Control
+                    id="usernameInput"
+                    value={username}
+                    onChange={(e) => {
+                        setUsername(e.target.value);
+                    }}
+                ></Form.Control>
+                <Form.Label htmlFor="passwordInput">Password</Form.Label>
+                <Form.Control
+                    id="passwordInput"
+                    type="password"
+                    value={password}
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                ></Form.Control>
+                <br />
+                <Button type="submit" onClick={handleLoginSubmit}>
+                    Login
+                </Button>
+            </Form>
+        );
     }
 }
